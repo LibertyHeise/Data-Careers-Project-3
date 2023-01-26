9b5c9136e2c8d86f57ba3f230a83b9e8b41cfc38
tallantj05 <jeremytallant@Jeremys-MBP.lan>
2023-01-25 20:41:48 -0600

    made changes to the files

--- app.py
+++ app.py
@@ -1,30 +1,84 @@
-from flask import Flask
+# from flask import Flask, render_template, jsonify
+# from pymongo import MongoClient
+# import json
+# from bson.json_util import dumps, loads
+
+# app = Flask(__name__)
+
+# # Connect to the MongoDB server
+# client = MongoClient()
+
+# # Choose the database you want to use
+# db = client.data
+
+# def get_data(data):
+#     data['_id'] = str(data['_id'])
+#     return data
+
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+
+
+# @app.route("/api/v1.0/states")
+# def states():
+#     data = [get_data(x) for x in db.states.find()]
+#     # Retrieve all documents from the "states" collection
+#     states_data = list(db.states.find())
+
+#     # json_data = json.dumps(states_data)
+
+#     # Return the data as a JSON object
+#     return jsonify(json.dumps(data))
+
+# @app.route("/api/v1.0/jobs")
+# def result():
+#     data2 = [get_data(x) for x in db.jobs.find()]
+#     # Retrieve all documents from the "jobs" collection
+#     jobs_data = list(db.jobs.find())
+
+#     # Return the data as a JSON object
+#     return jsonify(json.dumps(data2))
+
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5001)
+
+from flask import Flask, jsonify, render_template
 from pymongo import MongoClient
-from bson.json_util import dumps
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
-
-# Connect to the MongoDB server
 client = MongoClient()
-
-# Choose the database you want to use
 db = client.data
 
-@app.route("/states")
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/api/v1.0/states")
 def states():
-    # Retrieve all documents from the "clean_states" collection
-    states_data = list(db.states.find({}))
+    # Retrieve all documents from the "states" collection
+    states_data = list(db.states.find())
+
+    # Convert ObjectId to string
+    for state in states_data:
+        state['_id'] = str(state['_id'])
 
     # Return the data as a JSON object
-    return dumps(states_data)
+    return jsonify(states_data)
 
-@app.route("/jobs")
-def result():
-    # Retrieve all documents from the "result" collection
-    jobs_data = list(db.jobs.find({}))
+@app.route("/api/v1.0/jobs")
+def jobs():
+    # Retrieve all documents from the "jobs" collection
+    jobs_data = list(db.jobs.find())
+
+    # Convert ObjectId to string
+    for job in jobs_data:
+        job['_id'] = str(job['_id'])
 
     # Return the data as a JSON object
-    return dumps(jobs_data)
+    return jsonify(jobs_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
+
---
+++ static/js/logic.js
@@ -0,0 +1,43 @@
+// // Initialize the map
+// const map = L.map('map').setView([39.828175, -98.5795], 4);
+
+// // Add the OpenStreetMap tile layer
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+
+d3.json('http://127.0.0.1:5001/api/v1.0/states').then(
+    function (data) {
+        console.log(data);
+        
+
+
+    }
+);
+// // Fetch data from the Flask API
+// fetch('http://127.0.0.1:5001/api/v1.0/states', {
+//         method: "GET",
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//     })
+//     .then(response => {
+//         if(response.ok) {
+//             return response.json();
+//         }
+//         else {
+//             throw new Error(response.statusText);
+//         }
+//     })
+//     .then(data => {
+//         // Loop through the data and add markers for each state
+//         data.forEach(state => {
+//             L.marker([state.capital.latitude, state.capital.longitude])
+//                 .bindPopup(state.capital.name)
+//                 .addTo(map);
+//         });
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     });
+
